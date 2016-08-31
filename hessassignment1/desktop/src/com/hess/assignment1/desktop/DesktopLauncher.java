bc6568ee355cf2730e0beeb3aba3d8bbdb25a005
Raistlin Hess <rh5516@cs.ship.edu>
2016-08-31 13:07:40 -0400

    Initial Setup. .gitignore has been added, as well as the folders containing the libGDX workspace for Assignment 2.

---
+++ hessassignment1/desktop/src/com/hess/assignment1/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.hess.assignment1.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.hess.assignment1.hessassignment1;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new hessassignment1(), config);
+	}
+}
